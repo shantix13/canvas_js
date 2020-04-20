8fcc76ca11a5d204c7f8e78929cd401c500cea9f
shantix <shantix@spaceofjoy.ru>
2020-04-20 14:22:57 +0300

    Canvas Js: Second Lesson

--- script.js
+++ script.js
@@ -1,22 +1,44 @@
 let canvas = document.getElementById('c1'); // Get canvas element
 let ctx = canvas.getContext('2d'); // Get canvas context
 
-ctx.fillStyle = 'red';  // Defer element colour
-ctx.fillRect(100, 50, 150, 75); // Fill rectangle fillRect(x, y, width, height) in px black colour by default
+
+ctx.beginPath();
+ctx.moveTo(100, 100); // Позиционируем перо на первую точку
+ctx.lineTo(150, 150); // Задаем координаты линии
+ctx.strokeStyle = 'red'; // Задаем цвет линии
+ctx.lineWidth = 5; // Задаем толщину линии
+ctx.stroke(); // Даем линии графическую обводку
+
+ctx.lineTo(300, 50);  // Линия пойдет от последней нарисованной точки.
+ctx.stroke();
+
+ctx.beginPath();
+ctx.moveTo(200, 50); // Задали новые координаты пера
+ctx.lineTo(300, 50);
+ctx.strokeStyle = 'blue';
+ctx.lineWidth = 20;
+ctx.lineCap = 'round'; // Задает обрамление краев линии (round, square, butt - по-умолчанию)
+ctx.stroke(); // В этом случае обе линии будут синими. Canvas считает, что это все пас одним путем. Чтобы такого не совершать, нужно начинать
+// новый путь! Для этого существует метод beginPath(). Вставил beginPath(); Нужно применять перед каждым новым отрезком,
+// который должен отличаться по параметрам!
+
+ctx.clearRect(0, 0, 400, 200);
+
+// Draw Triangle
+
+ctx.beginPath();
+ctx.moveTo(50, 150);
+ctx.lineTo(150, 50);
+ctx.lineTo(200, 150);
+ctx.lineTo(50, 150);
+ctx.lineWidth = 5;
+ctx.lineCap = 'square';
+ctx.fillStyle = 'yellow';
+ctx.stroke();
+
+ctx.fill(); // fill triangle
+
+ctx.closePath(); // Метод замыкает путь автоматически
 
 
-ctx.fillStyle = 'blue';
-ctx.fillRect(150, 100, 100, 50);
-
-//ctx.clearRect(0, 0, 400, 200); // Erase Canvas element
-
-ctx.clearRect(150, 100, 50, 50);
-
-
-ctx.rect(5, 10, 100, 100); // Рисуем прямоугольник разных типов. Этой командой только задаем координаты
-ctx.strokeStyle = 'green'; // Задаем цвет заливки
-ctx.lineWidth = 10; // Задаем ширину линии обводки
-ctx.fillStyle = 'orange'; //
-ctx.fill(); // Заливаем прямоугольник
-ctx.stroke(); // Создаем обводку по заданным координатам.
 
